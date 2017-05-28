7c04abee22158694dc848e195a5ac09c39848777
Armin <kzardkoohi@gmail.com>
2017-05-27 19:57:15 -0500

    Added wireframe UI elements. Added back/forward/reload button functionality from mainstoryboard. Loaded test web URL.

--- iBrowse/iBrowseController.swift
+++ iBrowse/iBrowseController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  iBrowseController.swift
 //  iBrowse
 //
 //  Created by Kevin Armin Zardkoohi on 5/27/17.
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class iBrowseController: UIViewController {
 
+    var webView:WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        webView = WKWebView(frame: CGRect(x: 0, y: 60, width: view.frame.size.width, height: view.frame.size.height))
+        view.addSubview(webView)
+        let url:URL = URL(string: "https://google.com")!
+        let req:URLRequest = URLRequest(url: url)
+        webView.load(req)
     }
 
     override func didReceiveMemoryWarning() {
